ef140d07f98ee037a2d6496629033ceab3b4d868
Yeray <yeray.guzmanpadron@student.rocvf.nl>
2018-11-22 10:57:02 +0100

    Javascript made by Yeray

---
+++ js/mijnjavascript.js
@@ -0,0 +1,22 @@
+var a = 10;
+if (a > 9) {
+    console.log("Hallo A is groter dan 9 hoor");
+}
+else {
+    console.log("A is kleiner dan 9");
+}
+
+for (var i = 0; i <= 10; i++) {
+    console.log("I heeft nu de waarde " + i);
+}
+
+var mijnauto = {
+    merk: "Suzuki",
+    type: "Swift",
+    aantalWielen: 4,
+    kleur: "zwart",
+}
+console.log(mijnauto.merk);
+console.log(mijnauto.type);
+console.log(mijnauto.aantalWielen);
+console.log(mijnauto.kleur);
