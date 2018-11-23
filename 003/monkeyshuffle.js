de62f02641be67554bf4975df29a79217144204e
Yeray <yeray.guzmanpadron@student.rocvf.nl>
2018-11-23 13:55:19 +0100

    Javascript made by Yeray

---
+++ 003/monkeyshuffle.js
@@ -0,0 +1,15 @@
+var plaatjes = document.getElementsByTagName("img");
+var random = 0;
+var randomnumbers = [];
+
+while(randomnumbers.length < 9) {
+    random = Math.floor(Math.random() *9 ) +1;
+    if (randomnumbers.lastIndexOf(random) == -1) {
+        randomnumbers.push(random);
+    }
+}
+random = 0;
+for (var plaatje in plaatjes) {
+    plaatjes[plaatje].src = "img/aap" + randomnumbers[random] + ".jpg";
+    random++;
+}
